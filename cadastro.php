a5382c1a0b246c71f840797b01c2a88033724097
20112144110295 <20112144110295@PAR155983.ifrn.local>
2013-07-24 19:48:31 -0300

    Versão 1.1 - Jean & Brunno

---
+++ cadastro.php
@@ -0,0 +1,54 @@
+<html>
+	<head>
+		<title> Tex Produtos Naturais | Para uma Vida Saud�vel </title>
+                
+                <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+		<link href='http://fonts.googleapis.com/css?family=Stalinist+One' rel='stylesheet' type='text/css'>
+		<style type = "text/css">
+			body	{
+						margin: 0; 
+						padding: 0;
+						font: 12px/1.4 Verdana, Arial, Helvetica, sans-serif;
+						text-align: center;
+					}
+					
+			div.principal	{
+								width: 980px;
+								border-style: solid;
+								border-width: 1px;
+								margin: 0 auto;
+								text-align:left;
+							}
+			
+			div.imagem	{	
+							width:980px;
+							height:237px;
+							background-image: url("teste2.png");
+						}
+			ul#nav {
+				margin:0; 
+				padding:0;
+				color: Snow;
+				} 
+			ul#nav li { 
+				background:#000;
+				margin-right:5px;
+				display: inline;
+				list-style-type: none;
+				}
+			ul#nav li a {
+				color: #fff;
+				font-size: 11px;
+				font-weight: bold;
+				text-decoration: none;
+				text-transform: uppercase;
+				padding: 3px 15px;
+				}
+		</style>
+	</head>
+	<body>
+            
+		</div>
+	</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,89 @@
+<html>
+	<head>
+		<title> Tex Produtos Naturais | Para uma Vida Saud�vel </title>
+                
+                <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+		<link href='http://fonts.googleapis.com/css?family=Stalinist+One' rel='stylesheet' type='text/css'>
+		<style type = "text/css">
+			body	{
+						margin: 0; 
+						padding: 0;
+						font: 12px/1.4 Verdana, Arial, Helvetica, sans-serif;
+						text-align: center;
+					}
+					
+			div.principal	{
+								width: 980px;
+								border-style: solid;
+								border-width: 1px;
+								margin: 0 auto;
+								text-align:left;
+							}
+			
+			div.imagem	{	
+							width:980px;
+							height:237px;
+							background-image: url("teste2.png");
+						}
+			ul#nav {
+				margin:0; 
+				padding:0;
+				color: Snow;
+				} 
+			ul#nav li { 
+				background:#000;
+				margin-right:5px;
+				display: inline;
+				list-style-type: none;
+				}
+			ul#nav li a {
+				color: #fff;
+				font-size: 11px;
+				font-weight: bold;
+				text-decoration: none;
+				text-transform: uppercase;
+				padding: 3px 15px;
+				}
+		</style>
+	</head>
+	<body>
+		<div class = "principal">
+			<div class = "imagem">
+				<h1 style = "margin: 0px; font-family: 'Stalinist One', cursive;"> Tex Produtos Naturais </h1>
+			</div>
+			
+			
+			<ul id = "nav">
+				<li><a href="idex.php">home</a></li>
+				<li><a href="produtos.php">produtos</a></li>
+				<li><a href="contato">contato</a></li>
+				<li><a href="sobre">sobre</a></li>                      
+                                <li><a href="cadastro">Escritório Virtual</a></li>
+			</ul>
+			<h2> Os melhores produtos naturais do mercado </h2>
+			
+			<p> 
+				Tex Produtos Naturais oferece uma ampla gama de produtos de qualidade preparados a partir do que a natureza oferece de melhor.
+				Plantas medicinais com alto poder cura, com efic�cia compravada cientificamente.
+			</p>
+			
+			<img src = "aloe_vera.jpg" title = "Aloe Vera" alt = "Aloe Vera">
+			
+			<h2> Fornecedores Internacionais </h2>
+			<p> A Tex Produtos Naturais conta com dois fornecedores internacionais </p>
+			<img src = "foreverliving_logo.jpg" title = "Logo Forever" alt = "Logo Forever">
+			<p>
+				Forever Living Products (FLP), Forever Living Products International Inc. � uma empresa de plantio, 
+				processamento e distribui��o de produtos de aloe vera (suco de babosa da fam�lia "Aloe Barbadensis Miller") e 
+				derivados de mel. Produtos ligados a sa�de, higiene pessoal, est�tica, nutri��o e gerenciamento de peso, como: 
+				bebidas, aromaterapia, cosm�ticos, suplementos alimentares, fitoter�picos, perfumes, redu��o de gorduras, combo paks.
+			</p>
+			<img src = "herbalife_logo.jpg" title = "Logo Herbalife" alt = "Logo Forever" width = "375" height = "150">
+			<p>
+				A Herbalife � uma empresa multinacional de nutri��o, controle de peso e nutri��o externa. incorporada nas Ilhas Cayman1 
+				e com escrit�rio sede nos Estados Unidos.
+			</p>
+			<h5>Copyright &copy; 2013 - Text Produtos Naturais</h5>
+		</div>
+	</body>
+</html>
