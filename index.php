7f2f78421de0cc5e8df66ee2503531a9f923f93d
Teixeira <Teixeira@Teixeira-PC>
2013-07-31 16:43:36 -0300

    Foram feitas várias modificações. O banner foi alterado, assim como o CSS foi separado do código HTML

--- index.php
+++ index.php
@@ -1,89 +1,76 @@
 <html>
-	<head>
-		<title> Tex Produtos Naturais | Para uma Vida Saud�vel </title>
-                
-                <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-		<link href='http://fonts.googleapis.com/css?family=Stalinist+One' rel='stylesheet' type='text/css'>
-		<style type = "text/css">
-			body	{
-						margin: 0; 
-						padding: 0;
-						font: 12px/1.4 Verdana, Arial, Helvetica, sans-serif;
-						text-align: center;
-					}
-					
-			div.principal	{
-								width: 980px;
-								border-style: solid;
-								border-width: 1px;
-								margin: 0 auto;
-								text-align:left;
-							}
-			
-			div.imagem	{	
-							width:980px;
-							height:237px;
-							background-image: url("teste2.png");
-						}
-			ul#nav {
-				margin:0; 
-				padding:0;
-				color: Snow;
-				} 
-			ul#nav li { 
-				background:#000;
-				margin-right:5px;
-				display: inline;
-				list-style-type: none;
-				}
-			ul#nav li a {
-				color: #fff;
-				font-size: 11px;
-				font-weight: bold;
-				text-decoration: none;
-				text-transform: uppercase;
-				padding: 3px 15px;
-				}
-		</style>
-	</head>
-	<body>
-		<div class = "principal">
-			<div class = "imagem">
-				<h1 style = "margin: 0px; font-family: 'Stalinist One', cursive;"> Tex Produtos Naturais </h1>
-			</div>
-			
-			
-			<ul id = "nav">
-				<li><a href="idex.php">home</a></li>
-				<li><a href="produtos.php">produtos</a></li>
-				<li><a href="contato">contato</a></li>
-				<li><a href="sobre">sobre</a></li>                      
-                                <li><a href="cadastro">Escritório Virtual</a></li>
-			</ul>
-			<h2> Os melhores produtos naturais do mercado </h2>
-			
-			<p> 
-				Tex Produtos Naturais oferece uma ampla gama de produtos de qualidade preparados a partir do que a natureza oferece de melhor.
-				Plantas medicinais com alto poder cura, com efic�cia compravada cientificamente.
-			</p>
-			
-			<img src = "aloe_vera.jpg" title = "Aloe Vera" alt = "Aloe Vera">
-			
-			<h2> Fornecedores Internacionais </h2>
-			<p> A Tex Produtos Naturais conta com dois fornecedores internacionais </p>
-			<img src = "foreverliving_logo.jpg" title = "Logo Forever" alt = "Logo Forever">
-			<p>
-				Forever Living Products (FLP), Forever Living Products International Inc. � uma empresa de plantio, 
-				processamento e distribui��o de produtos de aloe vera (suco de babosa da fam�lia "Aloe Barbadensis Miller") e 
-				derivados de mel. Produtos ligados a sa�de, higiene pessoal, est�tica, nutri��o e gerenciamento de peso, como: 
-				bebidas, aromaterapia, cosm�ticos, suplementos alimentares, fitoter�picos, perfumes, redu��o de gorduras, combo paks.
-			</p>
-			<img src = "herbalife_logo.jpg" title = "Logo Herbalife" alt = "Logo Forever" width = "375" height = "150">
-			<p>
-				A Herbalife � uma empresa multinacional de nutri��o, controle de peso e nutri��o externa. incorporada nas Ilhas Cayman1 
-				e com escrit�rio sede nos Estados Unidos.
-			</p>
-			<h5>Copyright &copy; 2013 - Text Produtos Naturais</h5>
-		</div>
-	</body>
+    <head>
+            <title> Tex Produtos Naturais | Para uma Vida Saudável </title>
+            <link href='http://fonts.googleapis.com/css?family=Stalinist+One' rel='stylesheet' type='text/css'>
+            <link rel="stylesheet" type="text/css" href="standard.css">
+            <style type = "text/css">
+                    #imagens img{
+                                            border-radius: 20px;
+                                            border-style: solid;
+                                            border-color: Snow;
+                                            margin-top: 20px;
+                                            }
+                    #texto h2 	{
+                                            margin-top: 50px;
+                                            font-size:23px;
+                                            }
+            </style>
+    </head>
+    <body>
+        <div class = "principal">
+            <div style = "position:relative; width:980px; height:230px;">
+
+                <div class = "banner">
+                        <img src="imagens/perca-medo-ser-feliz-anamaria-767-11279.jpg" width="980px" height="236px" alt="Primeira Foto">
+                </div>
+
+
+                <ul id = "nav">
+                        <li> <a href = "index.php"> HOME </a> </li>
+                        <li> <a href = "produtos.php"> PRODUTOS </a> </li>
+                        <li> <a href = "contato.php"> CONTATO </a> </li>
+                        <li> <a href = "escritorio.php"> ESCRITORIO VIRTUAL </a> </li>
+                        <li> <a href = "sobre.html"> SOBRE </a></li>
+                </ul>
+
+                <form id="busca">
+                        <input type="text" value=" buscar" name="busca">
+                        <input type="submit" value="OK" id="submit">
+                </form>
+            </div>
+            <img src = "imagens/scan.png" width = "980px" height = "50px" title = "teste" alt = "teste">
+            <div id = "texto">
+                <h2> Os melhores produtos naturais do mercado </h2>
+
+                <p> 
+                        Tex Produtos Naturais oferece uma ampla gama de produtos de qualidade preparados a partir do que a natureza oferece de melhor.
+                        Plantas medicinais com alto poder cura, com eficácia compravada cientificamente.
+                </p>
+                <h2> Fornecedores Internacionais </h2>
+                <p> A Tex Produtos Naturais conta com dois fornecedores internacionais </p>
+
+                <p>
+                        Forever Living Products (FLP), Forever Living Products International Inc. é uma empresa de plantio, 
+                        processamento e distribuição de produtos de aloe vera (suco de babosa da família "Aloe Barbadensis Miller") e 
+                        derivados de mel. Produtos ligados a saúde, higiene pessoal, estética, nutrição e gerenciamento de peso, como: 
+                        bebidas, aromaterapia, cosméticos, suplementos alimentares, fitoterápicos, perfumes, redução de gorduras, combo paks.
+                </p>
+
+                <p>
+                        A Herbalife é uma empresa multinacional de nutrição, controle de peso e nutrição externa. incorporada nas Ilhas Cayman1 
+                        e com escritório sede nos Estados Unidos.
+                </p>
+            </div>
+
+            <div id = "imagens">
+                    <img src = "imagens/aloe_vera.jpg" title = "Aloe Vera" alt = "Aloe Vera" width = "394px" height = "220px" >
+                    <img src = "imagens/foreverliving_logo.jpg" title = "Logo Forever" alt = "Logo Forever">
+                    <img src = "imagens/herbalife_logo.jpg" title = "Logo Herbalife" alt = "Logo Forever" width = "375" height = "150">
+            </div>
+
+            <div id = "creditos">
+                    <p>Copyright &copy; 2013 - <b>Text</b> <span> Produtos Naturais </span> </p>
+            </div>
+        </div>
+    </body>
 </html>
