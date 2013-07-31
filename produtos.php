36e5afbaab4ee618680e5bd38ab0d98d0c8c91e6
Teixeira <Teixeira@Teixeira-PC>
2013-07-31 16:55:52 -0300

    A página produtos foi criado com um layout básico e a página sobre soi criada com o layout padrão

--- produtos.php
+++ produtos.php
@@ -1,8 +1,160 @@
+<html>
+    <head>
+        <title> Tex Produtos Naturais | Para uma Vida Saudável </title>
+        <link href='http://fonts.googleapis.com/css?family=Stalinist+One' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="standard.css">
+        <style type = "text/css">
+            #texto	{
+                            width: 700px;
+                            height: auto;
+                            float:left;
+                            background-color: Snow;
+                            margin-left: 20px;
+                            margin-right: 5px;
+                            text-align: justify;
+                            margin-bottom: 20px;
+                    }
+            #imagens{
+                            float: right;
+                            width: 225px;
+                            height: 500px;
+                    }
+            #texto h2 {
+                            margin-top: 50px;
+                            font-size:23px;
+                    }
+            #imagens {
+                            background:#00C5CD;
+                            height:500px;
+                            width:245px;
+                            float:right;
+                            margin-top:0;
+                            margin-bottom:50px;
+                            }
+            dl	{
+                            margin:0;
+                            padding:0;
+                    }
+            dd	{
+                            margin-left: 20px;
+                    }
+            .principal dl {
+                            width:195px;
+                            float:left;
+                            margin-bottom:30px;
+                            margin-right: 30px;
+                    }
+        </style>
+    </head>
+    <body>
+        <div class = "principal">
+            <div style = "position:relative; width:980px; height:230px;">
+                <div class = "banner">
+                        <img src="imagens/perca-medo-ser-feliz-anamaria-767-11279.jpg" width="980px" height="236px" alt="Primeira Foto" />
+                </div>
 
-<?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+                <ul id = "nav">
+                        <li> <a href = "index.php"> HOME </a> </li>
+                        <li> <a href = "produtos.php"> PRODUTOS </a> </li>
+                        <li> <a href = "contato.php"> CONTATO </a> </li>
+                        <li> <a href = "escritorio.php"> ESCRITORIO VIRTUAL </a> </li>
+                        <li> <a href = "sobre.html"> SOBRE </a></li>
+                </ul>
+
+                <form id="busca">
+                        <input type="text" value=" buscar" name="busca" />
+                        <input type="submit" value="OK" id="submit" />
+                </form>
+            </div>
+            <img src = "imagens/scan.png" width = "980px" height = "50px" title = "teste" alt = "teste">
+            <div id = "texto">
+                    <h2> Forever Living </h2>
+
+                    <p>
+                            Experience the difference of Forever Living’s products. Infused with 100% pure aloe vera gel and many other beneficial 
+                            botanicals, these products will give you something unexpected. Energy. Vibrancy. Wellness. You’ll notice it right 
+                            away—and others will too. The difference is only natural.
+                    </p>
+
+                    <dl>
+                            <dt><img src="imagens/forever/bee_pollen.jpg" alt="BEE POLLEN" title = "BEE POLLEN"/></dt>     
+                            <dd><a href="#">Bee Pollen</a></dd>
+                            <dd>Excellent supplement from the beehive</dd>
+                            <dd>Boosts energy</dd>    
+                            <dd>May enhance stamina</dd>
+                    </dl>
+
+                    <dl>
+                            <dt><img src="imagens/forever/bee_pollen.jpg" alt="BEE POLLEN" title = "BEE POLLEN"/></dt>     
+                            <dd><a href="#">Bee Pollen</a></dd>
+                            <dd>Excellent supplement from the beehive</dd>
+                            <dd>Boosts energy</dd>    
+                            <dd>May enhance stamina</dd>
+                    </dl>
+
+                    <dl>
+                            <dt><img src="imagens/forever/bee_pollen.jpg" alt="BEE POLLEN" title = "BEE POLLEN"/></dt>     
+                            <dd><a href="#">Bee Pollen</a></dd>
+                            <dd>Excellent supplement from the beehive</dd>
+                            <dd>Boosts energy</dd>    
+                            <dd>May enhance stamina</dd>
+                    </dl>
+
+                    <dl>
+                            <dt><img src="imagens/forever/bee_pollen.jpg" alt="BEE POLLEN" title = "BEE POLLEN"/></dt>     
+                            <dd><a href="#">Bee Pollen</a></dd>
+                            <dd>Excellent supplement from the beehive</dd>
+                            <dd>Boosts energy</dd>    
+                            <dd>May enhance stamina</dd>
+                    </dl>
+                    <hr style = "clear:both; border-width:1px; border-color: red;">
+                    <h2> Herbalife </h2>
+
+                    <p>
+                            Experience the difference of Forever Living’s products. Infused with 100% pure aloe vera gel and many other beneficial 
+                            botanicals, these products will give you something unexpected. Energy. Vibrancy. Wellness. You’ll notice it right 
+                            away—and others will too. The difference is only natural.
+                    </p>
+
+                    <dl>
+                            <dt><img src="imagens/herbalife/xtra_cal.png" alt="BEE POLLEN" title = "BEE POLLEN"/></dt>     
+                            <dd><a href="#">Xtra Cal</a></dd>
+                            <dd>50% da ingestão diária recomendada de Vitamina D</dd>
+                            <dd>40% da ingestão diária recomendada de Cálcio</dd>    
+                            <dd>57% da ingestão diária recomendada de Magnésio</dd>
+                    </dl>
+
+                    <dl>
+                            <dt><img src="imagens/herbalife/xtra_cal.png" alt="BEE POLLEN" title = "BEE POLLEN"/></dt>     
+                            <dd><a href="#">Xtra Cal</a></dd>
+                            <dd>50% da ingestão diária recomendada de Vitamina D</dd>
+                            <dd>40% da ingestão diária recomendada de Cálcio</dd>    
+                            <dd>57% da ingestão diária recomendada de Magnésio</dd>
+                    </dl>
+
+                    <dl>
+                            <dt><img src="imagens/herbalife/xtra_cal.png" alt="BEE POLLEN" title = "BEE POLLEN"/></dt>     
+                            <dd><a href="#">Xtra Cal</a></dd>
+                            <dd>50% da ingestão diária recomendada de Vitamina D</dd>
+                            <dd>40% da ingestão diária recomendada de Cálcio</dd>    
+                            <dd>57% da ingestão diária recomendada de Magnésio</dd>
+                    </dl>
+
+                    <dl>
+                            <dt><img src="imagens/herbalife/xtra_cal.png" alt="BEE POLLEN" title = "BEE POLLEN"/></dt>     
+                            <dd><a href="#">Xtra Cal</a></dd>
+                            <dd>50% da ingestão diária recomendada de Vitamina D</dd>
+                            <dd>40% da ingestão diária recomendada de Cálcio</dd>    
+                            <dd>57% da ingestão diária recomendada de Magnésio</dd>
+                    </dl>
+            </div>
+            <div id = "imagens">
+                    teste
+            </div>
+            <div id = "creditos">
+                    <p>Copyright &copy; 2013 - <b>Text</b> <span> Produtos Naturais </span> </p>
+            </div>
+        </div>
+    </body>
+</html>
