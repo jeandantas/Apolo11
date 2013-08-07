1707749f553ad71ae04d5b3cea4fdb6c8fc66e38
20112144110295 <20112144110295@PAR155983.ifrn.local>
2013-08-07 20:43:08 -0300

    alteração da página contato.

--- contato.php
+++ contato.php
@@ -8,67 +8,82 @@
             <div style = "position:relative; width:980px; height:230px;">
 
                 <div class = "banner">
-                        <img src="imagens/perca-medo-ser-feliz-anamaria-767-11279.jpg" width="980px" height="236px" alt="Primeira Foto">
+                    <img src="imagens/perca-medo-ser-feliz-anamaria-767-11279.jpg" width="980px" height="236px" alt="Primeira Foto">
                 </div>
 
                 <ul id = "nav">
-                        <li> <a href = "index.php"> HOME </a> </li>
-                        <li> <a href = "produtos.php"> PRODUTOS </a> </li>
-                        <li> <a href = "contato.php"> CONTATO </a> </li>
-                        <li> <a href = "escritorio.php"> ESCRITORIO VIRTUAL </a> </li>
-                        <li> <a href = "sobre.html"> SOBRE </a></li>
+                    <li> <a href = "index.php"> HOME </a> </li>
+                    <li> <a href = "produtos.php"> PRODUTOS </a> </li>
+                    <li> <a href = "contato.php"> CONTATO </a> </li>
+                    <li> <a href = "escritorio.php"> ESCRITORIO VIRTUAL </a> </li>
+                    <li> <a href = "sobre.html"> SOBRE </a></li>
                 </ul>
                 <form id="busca">
-                        <input type="text" value=" buscar" name="busca" />
-                        <input type="submit" value="OK" id="submit" />
+                    <input type="text" value=" buscar" name="busca" />
+                    <input type="submit" value="OK" id="submit" />
                 </form>
             </div>
 
             <img src = "imagens/scan.png" width = "980px" height = "50px" title = "teste" alt = "teste">
 
             <div id = "texto">
-                    <form name="nomedoform" action="xxxx" 
-method="xxxx" >
-         
-          <table>
-            <tr> 
-            <th colspan="2">Formul&aacute;rio 
-            de contato</th>
-            </tr>
-            <tr> 
-                <td><label>Nome</label></td>
-                <td><input name="nome" type="text" size="33" maxlength="1000"></td>
-            </tr>
-            <tr> 
-            <td><label>*e-mail</label></td>
-            <td><input name="email" type="text" size="33" maxlength="1000"></td>
-            </tr>
-            <tr> 
-            <td><label>Assunto</label></td>
-            <td><input name="subject" type="text" size="33" maxlength="1000"></td>
-            </tr>
-            <tr> 
-            <td><label>*Coment&aacute;rios</label>
-            </td>
-            <td><textarea name="comentario" cols="25" rows="7" >
-           </textarea>
-            </td>
-            </tr>
-            <tr> 
-            <td><input name="submit" type="submit" value= "Enviar" class="botao">
-            </td>
-            <td>*Campos de preenchimento obrigat&oacute;rio</td>
-            </tr>
-           </table>
-            </form>
-         </div>
+                <form name="nomedoform" action="xxxx" 
+                      method="xxxx" >
+                           <?php
+
+
+                          $nome = " "; //pega o nome do remetente
+                          $email = ""; //pega o email do remetente
+                          $receptor = ""; //seu email
+                          $mensagem = ""; //mensagem
+                          $assunto = ""; //assunto
+                          
+
+                          if (($nome == "") || ($email == "") || ($assunto == "") || ($mensagem == "")) {
+                              echo "Atencao! Todos os campos do formulario devem ser preenchidos.";
+                         
+                          ?>
+
+
+                    <table>
+                        <tr> 
+                            <th colspan="2">Formul&aacute;rio 
+                                de contato</th>
+                        </tr>
+                        <tr> 
+                            <td><label>Nome</label></td>
+                            <td><input name="nome" type="text" size="33" maxlength="1000"></td>
+                        </tr>
+                        <tr> 
+                            <td><label>*e-mail</label></td>
+                            <td><input name="email" type="text" size="33" maxlength="1000"></td>
+                        </tr>
+                        <tr> 
+                            <td><label>Assunto</label></td>
+                            <td><input name="subject" type="text" size="33" maxlength="1000"></td>
+                        </tr>
+                        <tr> 
+                            <td><label>*Coment&aacute;rios</label>
+                            </td>
+                            <td><textarea name="comentario" cols="25" rows="7" >
+                                </textarea>
+                            </td>
+                        </tr>
+                        <tr> 
+                            <td><input name="submit" type="submit" value= "Enviar" class="botao">
+                            </td>
+                            <td>*Campos de preenchimento obrigat&oacute;rio</td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
 
             <div id = "imagens">
-                 
+
             </div>
 
             <div id = "creditos">
-                    <p>Copyright &copy; 2013 - <b>Text</b> <span> Produtos Naturais </span> </p>
+                <p>Copyright &copy; 2013 - <b>Text</b> <span> Produtos Naturais </span> </p>
             </div>
         </div>
     </body>
