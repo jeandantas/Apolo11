8857d23ed6eebd856b95c407d9a13896f8164634
Teixeira <Teixeira@Teixeira-PC>
2013-09-04 17:55:57 -0300

    Tela de logon adicionada.

--- escritorio.php
+++ escritorio.php
@@ -2,6 +2,7 @@
 	<head>
 		<title> Standard Document </title>
 		<link rel="stylesheet" type="text/css" href="standard.css">
+                <link rel="stylesheet" type="text/css" href="escritorio.css">
 	</head>
 	<body>
 		<div class = "principal">
@@ -27,11 +28,26 @@
 			<img src = "imagens/scan.png" width = "980px" height = "50px" title = "teste" alt = "teste">
 			
 			<div id = "texto">
-				<p> Insiera aqui a conteudo principal da página </p>
+                            <h2> Seja Bem Vindo ao Escritorio Virtual </h2>
+                            <form id = 'logon'>
+                                <fieldset>
+                                    <legend id = 'entrar'> Entrar </legend>
+                                    <label id = 'texto01'> Login </label>
+                                    <input type = 'text' id = 'field_user'> <br>
+                                    <label id = 'texto02'> Password </label>
+                                    <input type = 'password' id = 'field_pass'>
+                                    <input type = 'submit' id = 'confirm' value = 'Entrar' >
+
+                                    <div id = 'opcoes'>
+                                        <a href = '#'> Esqueceu a senha? </a> <br>
+                                        <a href = 'cadastro.php'> N&abreve;o &eacute; Cadastrado? </a>
+                                    </div>
+                                </fieldset>
+                            </form>
 			</div>
 			
 			<div id = "imagens">
-				<p> Menu Lateral </p>
+				
 			</div>
 			
 			<div id = "creditos">
