42451cc6fedd7c2c5155a6af34db3f7a406ca427
20111144110106 <20111144110106@PAR155981.ifrn.local>
2013-09-10 20:27:22 -0300

    Criação das classes de domínio Cliente e Compra

---
+++ dominio/Cliente.php
@@ -0,0 +1,101 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Cliente
+ *
+ * @author 20111144110106
+ */
+class Cliente {
+    private $nome;
+    private $sobrenome;
+    private $cpf;
+    private $email;
+    private $endereco;
+    private $senha;
+    private $idCliente;
+    private $ehVisitante;
+    
+    function __construct($nome, $sobrenome, $cpf, $email, $endereco, $senha, $idCliente, $ehVisitante) {
+        $this->nome = $nome;
+        $this->sobrenome = $sobrenome;
+        $this->cpf = $cpf;
+        $this->email = $email;
+        $this->endereco = $endereco;
+        $this->senha = $senha;
+        $this->idCliente = $idCliente;
+        $this->ehVisitante = $ehVisitante;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function getSobrenome() {
+        return $this->sobrenome;
+    }
+
+    public function setSobrenome($sobrenome) {
+        $this->sobrenome = $sobrenome;
+    }
+
+    public function getCpf() {
+        return $this->cpf;
+    }
+
+    public function setCpf($cpf) {
+        $this->cpf = $cpf;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function getEndereco() {
+        return $this->endereco;
+    }
+
+    public function setEndereco($endereco) {
+        $this->endereco = $endereco;
+    }
+
+    public function getSenha() {
+        return $this->senha;
+    }
+
+    public function setSenha($senha) {
+        $this->senha = $senha;
+    }
+
+    public function getIdCliente() {
+        return $this->idCliente;
+    }
+
+    public function setIdCliente($idCliente) {
+        $this->idCliente = $idCliente;
+    }
+
+    public function getEhVisitante() {
+        return $this->ehVisitante;
+    }
+
+    public function setEhVisitante($ehVisitante) {
+        $this->ehVisitante = $ehVisitante;
+    }
+
+
+}
+
+?>
---
+++ dominio/Compra.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Compra
+ *
+ * @author 20111144110106
+ */
+class Compra {
+    private $compra;
+    private $data;
+    private $hora;
+    private $formaPagamento;
+    private $descricao;
+    private $idCliente;
+    
+    function __construct($compra, $data, $hora, $formaPagamento, $descricao, $idCliente) {
+        $this->compra = $compra;
+        $this->data = $data;
+        $this->hora = $hora;
+        $this->formaPagamento = $formaPagamento;
+        $this->descricao = $descricao;
+        $this->idCliente = $idCliente;
+    }
+    public function getCompra() {
+        return $this->compra;
+    }
+
+    public function setCompra($compra) {
+        $this->compra = $compra;
+    }
+
+    public function getData() {
+        return $this->data;
+    }
+
+    public function setData($data) {
+        $this->data = $data;
+    }
+
+    public function getHora() {
+        return $this->hora;
+    }
+
+    public function setHora($hora) {
+        $this->hora = $hora;
+    }
+
+    public function getFormaPagamento() {
+        return $this->formaPagamento;
+    }
+
+    public function setFormaPagamento($formaPagamento) {
+        $this->formaPagamento = $formaPagamento;
+    }
+
+    public function getDescricao() {
+        return $this->descricao;
+    }
+
+    public function setDescricao($descricao) {
+        $this->descricao = $descricao;
+    }
+
+    public function getIdCliente() {
+        return $this->idCliente;
+    }
+
+    public function setIdCliente($idCliente) {
+        $this->idCliente = $idCliente;
+    }
+}
+
+?>
